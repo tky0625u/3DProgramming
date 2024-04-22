0a30d9b8a9a253739aaba5d33e9a72208a2bd4a2
tky0625u <kd1370302@st.kobedenshi.ac.jp>
2024-04-22 15:02:39 +0900

    Camera
    Cameraコミット

--- L01_Camera01/Src/Application/main.cpp
+++ L01_Camera01/Src/Application/main.cpp
@@ -64,6 +64,14 @@ void Application::PreUpdate()
 // ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// /////
 void Application::Update()
 {
+	//カメラ行列の更新
+	{
+		Math::Matrix _localPos = Math::Matrix::CreateTranslation(0, 6.0f, 0);
+
+		//カメラの「ワールド行列」を作成し、適応させる
+		Math::Matrix _worldMat = _localPos;
+		m_spCamera->SetCameraMatrix(_worldMat);
+	}
 }
 
 // ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// /////
@@ -120,8 +128,10 @@ void Application::Draw()
 	// 陰影のあるオブジェクト(不透明な物体や2Dキャラ)はBeginとEndの間にまとめてDrawする
 	KdShaderManager::Instance().m_StandardShader.BeginLit();
 	{
-		Math::Matrix _mat = Math::Matrix::CreateTranslation(0, 0, 5);
+		Math::Matrix _mat = Math::Matrix::CreateTranslation(m_pos.x,m_pos.y,m_pos.z);
 		KdShaderManager::Instance().m_StandardShader.DrawPolygon(*m_spPoly, _mat);
+
+		KdShaderManager::Instance().m_StandardShader.DrawModel(*m_spModel);
 	}
 	KdShaderManager::Instance().m_StandardShader.EndLit();
 
@@ -181,9 +191,9 @@ bool Application::Init(int w, int h)
 	// フルスクリーン確認
 	//===================================================================
 	bool bFullScreen = false;
-	if (MessageBoxA(m_window.GetWndHandle(), "フルスクリーンにしますか？", "確認", MB_YESNO | MB_ICONQUESTION | MB_DEFBUTTON2) == IDYES) {
-		bFullScreen = true;
-	}
+	//if (MessageBoxA(m_window.GetWndHandle(), "フルスクリーンにしますか？", "確認", MB_YESNO | MB_ICONQUESTION | MB_DEFBUTTON2) == IDYES) {
+	//	bFullScreen = true;
+	//}
 
 	//===================================================================
 	// Direct3D初期化
@@ -234,6 +244,14 @@ bool Application::Init(int w, int h)
 	//===================================================================
 	m_spPoly = std::make_shared<KdSquarePolygon>();
 	m_spPoly->SetMaterial("Asset/Data/LessonData/Character/hamu.png");
+
+	//===================================================================
+	// 地形初期化初期化
+	//===================================================================
+	m_spModel = std::make_shared<KdModelData>();
+	m_spModel->Load("Asset/Data/LessonData/Terrain/Terrain.gltf");
+
+	m_pos = { 0,0,5 };
 
 	return true;
 }
--- L01_Camera01/Src/Application/main.h
+++ L01_Camera01/Src/Application/main.h
@@ -57,6 +57,11 @@ private:
 	//板ポリゴン
 	std::shared_ptr<KdSquarePolygon>m_spPoly = nullptr;
 
+	//地形モデル
+	std::shared_ptr<KdModelData>m_spModel = nullptr;
+
+	Math::Vector3 m_pos;
+
 //=====================================================
 // シングルトンパターン
 //=====================================================
